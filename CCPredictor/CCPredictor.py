a9f3da971b9084c39f4555e86c96ccc79afb2d99
awstone <alexstone977@gmail.com>
2018-01-27 22:54:45 -0500

    ditching DL plan

--- CCPredictor/CCPredictor.py
+++
@@ -1,8 +0,0 @@
-from SentimentNN.encoder import Model
-
-model = Model()
-file = open('RedditScraper/Output.txt', 'r')
-text = file.readlines()
-print(text)
-#text_features = model.transform(text)
-
